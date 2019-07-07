2431f5ce126874ffb57e297af816da108e5e1ab7
Devin Jordao <devinjordao@gmail.comgit config --global user.name Devin>
2019-07-07 00:33:07 +0000

    Handle Form submission and removed pagination

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,10 +1,21 @@
 class PlacesController < ApplicationController
 
   def index
-    @places = Place.all.paginate(page: params[:page], per_page: 2)
+    @places = Place.all
   end
 
   def new
     @place = Place.new
   end
+
+  def create
+    Place.create(place_params)
+  end
+
+  private
+
+  def place_params
+    params.require(:place).permit(:name, :description, :address)
+  end
+
 end
