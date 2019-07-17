f685e662c85fd7ee1a5966849c6edd9e44a85db3
Devin Jordao <devinjordao@gmail.comgit config --global user.name Devin>
2019-07-17 02:14:27 +0000

    Updating the place data

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_07_015322) do
+ActiveRecord::Schema.define(version: 2019_07_08_212229) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "photos", force: :cascade do |t|
+    t.text "caption"
+    t.bigint "place_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["place_id"], name: "index_photos_on_place_id"
+  end
 
   create_table "places", force: :cascade do |t|
     t.string "name"
@@ -37,4 +45,5 @@ ActiveRecord::Schema.define(version: 2019_07_07_015322) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "photos", "places"
 end
